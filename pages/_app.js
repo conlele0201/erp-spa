57511ff79aca7eaebdcf16753832b677d07490c5
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-04 08:46:27 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,11 @@
 // pages/_app.js
-import "../styles/sidebar.css";
+import "../styles/globals.css";      // CSS chung
+import Layout from "../components/Layout";   // Layout sidebar
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
