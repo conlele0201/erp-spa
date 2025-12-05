b8b05ec3343369d30b024341527b45fc7a91eeea
conlele0201 <129933655+conlele0201@users.noreply.github.com>
2025-12-06 05:51:33 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,9 @@
 // pages/_app.js
-import { useRouter } from "next/router";
-import Layout from "../components/Layout";
 import "../styles/globals.css";
-import "../styles/sidebar.css"; // 👈 THÊM DÒNG NÀY ĐỂ ÁP CSS CHO SIDEBAR
+import "../styles/sidebar.css"; // import sidebar dùng chung
+import Layout from "../components/Layout";
 
 function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-
-  // Trang dashboard (/) dùng layout riêng, KHÔNG bọc Layout chung
-  if (router.pathname === "/") {
-    return <Component {...pageProps} />;
-  }
-
-  // Các trang khác (khách hàng, lịch hẹn, ...) bọc trong Layout + sidebar mới
   return (
     <Layout>
       <Component {...pageProps} />
